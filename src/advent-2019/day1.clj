f1c155a96090e08b9a3277bf220ce8c1f0758b00
Will Acton <will@willacton.com>
2019-12-01 10:14:38 -0800

    tangling works

---
+++ src/advent-2019/day1.clj
@@ -0,0 +1,33 @@
+(ns advent-2019.day1)
+(require '[clojure.java.io :as io])
+
+(require '[clojure.string :as string])
+
+(def input (-> (io/resource "day1.input")
+               (slurp)
+               (string/split #"\n")
+               (->> (map #(Integer/parseInt %)))))
+
+(defn fuel [mass]
+  (-> mass (/ 3) (Math/floor) (- 2) (int)))
+
+(def part1 (transduce (map fuel) + input))
+
+(println part1)
+
+(defn fuel2
+  ([mass]
+   (fuel2 (fuel mass) 0))
+  ([mass sum]
+   (if (<= mass 0)
+     sum
+     (recur (fuel mass)
+            (+ sum mass)))))
+
+(assert (= (fuel2 1969) 966))
+
+(assert (= (fuel2 100756) 50346))
+
+(def part2 (transduce (map fuel2) + input))
+
+(println part2)
